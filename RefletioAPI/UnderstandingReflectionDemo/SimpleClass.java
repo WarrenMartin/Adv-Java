ceb64a12faf58a0b50257abc4734b5e11eac1902
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-08-07 15:12:34 +0530

    Create SimpleClass.java

---
+++ RefletioAPI/UnderstandingReflectionDemo/SimpleClass.java
@@ -0,0 +1,13 @@
+public class SimpleClass {
+    public String methodOne() {
+        return "This is method one.";
+    }
+
+    public String methodTwo() {
+        return "This is method two.";
+    }
+
+    public String methodThree() {
+        return "This is method three.";
+    }
+}

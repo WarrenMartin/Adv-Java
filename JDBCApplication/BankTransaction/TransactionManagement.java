49c8baf7ce74e6371d83324a98167146408fc44a
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-08-12 22:50:24 +0530

    Create TransactionManagement.java

---
+++ JDBCApplication/BankTransaction/TransactionManagement.java
@@ -0,0 +1,84 @@
+
+package jdbc_demo;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class TxMgmt {
+    
+    public static void main(String[] args) throws Exception  {
+        
+        DBConnection dbcon=new DBConnection();
+        Connection con=dbcon.getConnection();
+        
+        if(con!=null){
+            System.out.println("Connection established");
+        }
+        else{
+            System.out.println("Connection failed");
+        }
+        
+         Scanner in=new Scanner(System.in);
+        con.setAutoCommit(false);  // by default - commit is true
+        
+        System.out.println("Enter Source Account Number");
+        int src=in.nextInt();
+        System.out.println("Enter destination account");
+        int dest=in.nextInt();
+        System.out.println("Enter amount to be transferred: ");
+        double amount= in.nextDouble();
+        
+        Statement st=con.createStatement();
+        // Now you need to withdraw from the Source account so we wil write update
+        
+        String wq1= "update accounts set balance=balance - "+amount+ "where accno =  "+src;
+        st.addBatch(wq1);
+        String dq1="update accounts set balance=balance + "+amount+ "where accno = "+dest;
+        st.addBatch(dq1);
+        
+        
+        
+        // execute the batch
+        int[] res= st.executeBatch();
+        
+        // logic for commit and rollback
+        boolean flag=false;
+        for (int i = 0; i < res.length; i++) {
+            if(res[i] ==0){
+                flag=true;
+                break;
+            }          
+        }
+        
+        if(flag==true){
+            //one of the query failed 
+            con.rollback();
+            System.out.println("Transaction rolled back");
+        }
+        else{
+            con.commit();
+            System.out.println("Transaction commited");
+        }
+        
+        
+        
+        
+        
+   
+        
+        
+         
+        
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
